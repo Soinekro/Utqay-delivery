e070c43d64e947a245378ba7e8a964a987620edb
Elder <echumaceroj@ucvvirtual.edu.pe>
2021-06-15 15:49:01 -0500

    utqay

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+        <div class=" h-52 w-full sm:h-80 lg:h-96 bg-left bg-no-repeat bg-cover" style="background-image: url({{asset('img/fondoutqay.jpg')}})">
+        <div class=" font-bold text-base text-center pt-32 sm:text-lg font-mono md:text-3xl ">Bienvenido a Utqay</div>
+        <p class="m-0 text-xs text-center md:pt-2">lo que pidas, al Instante</p>
+        </div>
+</x-app-layout>
