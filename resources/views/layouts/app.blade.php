cb17b110cd075d42f8e33756436cae59a89dfa96
Elder <echumaceroj@ucvvirtual.edu.pe>
2021-06-16 19:52:55 -0500

    arreglando fondo de footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,49 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        {{-- Icon --}}
-        <link rel="shortcut icon" href="{{asset('img/utqay.icon.jpg')}}" type="image/x-icon">
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- Icon --}}
+    <link rel="shortcut icon" href="{{ asset('img/utqay.icon.jpg') }}" type="image/x-icon">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        @livewireStyles
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+    @livewireStyles
 
-        <div class="min-h-screen bg-gray-400">
-            @livewire('navigation-menu')
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-gray-100 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+<body class="font-sans antialiased">
+    <x-jet-banner />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+    <div class="min-h-screen bg-gray-300">
+        @livewire('navigation-menu')
 
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-gray-100 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+    <div class=" bg-blue-800">
         @include('layouts.footer')
+    </div>
 
-        @stack('modals')
 
-        @livewireScripts
-    </body>
+    @stack('modals')
+
+    @livewireScripts
+</body>
+
 </html>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,110 +1,112 @@
-<div class="container m-auto">
+<div class="m-auto">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
 
-  <footer class="text-blue-500 body-font">
-    <div
-      class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-      <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-        <a href="" class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          <i class="fas fa-truck fa-lg text-purple-500"></i>
-          <span class="ml-3 text-xl">Utqay Delivery</span>
-        </a>
-        <p class="mt-2 text-sm text-gray-700">Lo pides, Lo traemos enseguida</p>
-      </div>
-      <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
-        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-          <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-          <nav class="list-none mb-10">
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">First Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Second Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Third Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-            </li>
-          </nav>
+    <footer class="text-blue-500 body-font">
+        <div
+            class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+            <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+                <a href=""
+                    class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                    <i class="fas fa-truck fa-lg text-blue-500"></i>
+                    <span class="ml-3 text-xl">Utqay Delivery</span>
+                </a>
+                <p class="mt-2 text-sm text-gray-100">Lo pides, Lo traemos enseguida</p>
+            </div>
+            <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+                <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                    <nav class="list-none mb-10">
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">First Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Second Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Third Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Fourth Link</a>
+                        </li>
+                    </nav>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                    <nav class="list-none mb-10">
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">First Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Second Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Third Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Fourth Link</a>
+                        </li>
+                    </nav>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                    <nav class="list-none mb-10">
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">First Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Second Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Third Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Fourth Link</a>
+                        </li>
+                    </nav>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                    <nav class="list-none mb-10">
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">First Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Second Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Third Link</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-white hover:text-gray-800">Fourth Link</a>
+                        </li>
+                    </nav>
+                </div>
+            </div>
         </div>
-        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-          <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-          <nav class="list-none mb-10">
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">First Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Second Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Third Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-            </li>
-          </nav>
+        <div class="bg-gray-100">
+            <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+                <p class="text-gray-500 text-sm text-center sm:text-left">© 2021 Copyright:
+                    <a href="" class="text-gray-600 ml-1"
+                        target="_blank">Utqay-Delivery</a>
+                </p>
+                <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                    <a href="https://www.facebook.com/UtqaytuDelivery" class="text-blue-600">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="" class="ml-3 text-blue-400">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="" class="ml-3 text-blue-500">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                    <a href="" class="ml-3 text-red-500">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                    <a href="https://www.instagram.com/utqay.delivery/" class="ml-3 text-pink-300">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    </span>
+            </div>
         </div>
-        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-          <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-          <nav class="list-none mb-10">
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">First Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Second Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Third Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-            </li>
-          </nav>
-        </div>
-        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-          <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-          <nav class="list-none mb-10">
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">First Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Second Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Third Link</a>
-            </li>
-            <li>
-              <a href="" class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-            </li>
-          </nav>
-        </div>
-      </div>
-    </div>
-    <div class="bg-gray-100">
-      <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-        <p class="text-gray-500 text-sm text-center sm:text-left">© 2021 Copyright:
-          <a href="https://www.tailwind-elements.com/" class="text-gray-600 ml-1" target="_blank">Utqay-Delivery</a>
-        </p>
-        <sp an class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-          <a href="" class="text-blue-600">
-            <i class="fab fa-facebook-f"></i>
-          </a>
-          <a href="" class="ml-3 text-blue-400">
-            <i class="fab fa-twitter"></i>
-          </a>
-          <a href="" class="ml-3 text-blue-500">
-            <i class="fab fa-linkedin-in"></i>
-          </a>
-          <a href="" class="ml-3 text-red-500">
-            <i class="fab fa-youtube"></i>
-          </a>
-          <a href="" class="ml-3 text-pink-300">
-            <i class="fab fa-instagram"></i>
-          </a>
-        </span>
-      </div>
-    </div>
-  </footer>
+    </footer>
 </div>
